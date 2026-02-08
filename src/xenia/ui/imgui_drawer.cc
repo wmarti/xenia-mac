76f822827956df40d27682ba8e6815244c411d32
Will Martin <wmarti@bu.edu>
2026-02-09 01:42:42 +0900

    [Qt/macOS] Tighten args and wire surface_mac build
    (cherry picked from commit 62896bf718d5c14925b139dd3f34903da4712719)

--- src/xenia/ui/imgui_drawer.cc
+++ src/xenia/ui/imgui_drawer.cc
@@ -604,7 +604,7 @@ void ImGuiDrawer::SetImmediateDrawer(ImmediateDrawer* new_immediate_drawer) {
   if (immediate_drawer_) {
     GetIO().Fonts->TexID = reinterpret_cast<ImTextureID>(nullptr);
     font_texture_.reset();
-
+    locked_achievement_icon_.reset();
     notification_icon_textures_.clear();
   }
   immediate_drawer_ = new_immediate_drawer;
