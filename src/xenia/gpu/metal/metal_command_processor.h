2060fb7cf96dcb1397d1e717093adc4a7fe8e1c6
Will Martin <wmarti@bu.edu>
2026-02-12 21:28:36 +0900

    [Metal/CPU/P1] Gate primary-buffer ends on compile pressure

--- src/xenia/gpu/metal/metal_command_processor.h
+++ src/xenia/gpu/metal/metal_command_processor.h
@@ -167,6 +167,7 @@ class MetalCommandProcessor : public CommandProcessor {
   // Command buffer management
   void BeginCommandBuffer();
   void EndCommandBuffer();
+  bool CanEndSubmissionImmediately();
   void WaitForPendingCompletionHandlers();
   void ProcessCompletedSubmissions();
   bool EnsureDrawRingCapacity();
