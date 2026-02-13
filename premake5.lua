9220ed24a606ab504dbb1f66ee93ee5aaba7fdfa
Will Martin <wmarti@bu.edu>
2026-02-14 01:05:13 +0900

    [Build/macOS] Add FFmpeg x86 init sources for Mac-x86_64

--- premake5.lua
+++ premake5.lua
@@ -796,6 +796,34 @@ workspace("xenia")
         })
       filter({})
   end
+  -- The generated FFmpeg premake files currently don't include macOS x86_64 in
+  -- their x86 source filters. Add the x86 init sources here so the symbols used
+  -- by common codec/util code are available when targeting Mac-x86_64.
+  if os.istarget("macosx") then
+    local ffmpeg_dir = "third_party/FFmpeg"
+    project("libavcodec")
+      filter("platforms:Mac-x86_64")
+        files({
+          ffmpeg_dir .. "/libavcodec/x86/constants.c",
+          ffmpeg_dir .. "/libavcodec/x86/dct_init.c",
+          ffmpeg_dir .. "/libavcodec/x86/fdctdsp_init.c",
+          ffmpeg_dir .. "/libavcodec/x86/fft_init.c",
+          ffmpeg_dir .. "/libavcodec/x86/idctdsp_init.c",
+          ffmpeg_dir .. "/libavcodec/x86/mpegaudiodsp.c",
+          ffmpeg_dir .. "/libavcodec/x86/fdct.c",
+        })
+      filter({})
+    project("libavutil")
+      filter("platforms:Mac-x86_64")
+        files({
+          ffmpeg_dir .. "/libavutil/x86/cpu.c",
+          ffmpeg_dir .. "/libavutil/x86/fixed_dsp_init.c",
+          ffmpeg_dir .. "/libavutil/x86/float_dsp_init.c",
+          ffmpeg_dir .. "/libavutil/x86/imgutils_init.c",
+          ffmpeg_dir .. "/libavutil/x86/lls_init.c",
+        })
+      filter({})
+  end
   include("third_party/fmt.lua")
   include("third_party/glslang-spirv.lua")
   include("third_party/imgui.lua")
