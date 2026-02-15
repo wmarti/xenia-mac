c6e10b771c1914700e70c2d093c7a0974309c7ed
Will Martin <wmarti@bu.edu>
2026-02-15 16:12:39 +0900

    [iOS/Memory] Remove redundant executable icache flush

--- src/xenia/base/memory_posix.cc
+++ src/xenia/base/memory_posix.cc
@@ -329,11 +329,6 @@ bool QueryProtect(void* base_address, size_t& length, PageAccess& access_out) {
   const bool can_read = (prot & VM_PROT_READ) != 0;
   const bool can_write = (prot & VM_PROT_WRITE) != 0;
   const bool can_execute = (prot & VM_PROT_EXECUTE) != 0;
-    
-    if (can_execute) {
-        sys_icache_invalidate(base_address, length);
-    }
-
   if (can_write) {
     access_out =
         can_execute ? PageAccess::kExecuteReadWrite : PageAccess::kReadWrite;
